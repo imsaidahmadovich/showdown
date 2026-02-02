56aa16e47bcf7d694026a10f52248d88ed52aa5b
imsaidahmadovich <imsaidahmadovich@gmail.com>
2026-02-02 20:46:50 +0500

    ads
    ads

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -16,7 +16,27 @@ import type { PlayerStatistic } from '@/lib/data';
 import { useToast } from '@/hooks/use-toast';
 import { cn } from '@/lib/utils';
 import type { ElementType } from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        <meta name="google-adsense-account" content="ca-pub-XXXX" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6493634839455307"
+     crossorigin="anonymous"></script>
+      </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
 type ShopItem = {
     id: string;
     name: string;
