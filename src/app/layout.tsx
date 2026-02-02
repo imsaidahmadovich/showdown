2cc7d44da353b37d95329d7c7502f34424fdaac4
imsaidahmadovich <imsaidahmadovich@gmail.com>
2026-02-02 20:28:27 +0500

    Add Google Ads script to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,3 +38,5 @@ export default function RootLayout({
     </html>
   );
 }
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6493634839455307"
+     crossorigin="anonymous"></script>
