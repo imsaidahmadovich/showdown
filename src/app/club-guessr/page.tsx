54f148da27b0c7d22a0853ae39b7061b8985b3cb
imsaidahmadovich <imsaidahmadovich@gmail.com>
2026-02-03 05:21:00 +0500

    Remove Google Ads script from ClubGuessr page
    Removed Google Ads script from the page.

--- src/app/club-guessr/page.tsx
+++ src/app/club-guessr/page.tsx
@@ -264,6 +264,5 @@ function ClubGuessrPage() {
     </div>
   );
 }
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6493634839455307"
-     crossorigin="anonymous"></script>
+
 export default withAuth(ClubGuessrPage);
