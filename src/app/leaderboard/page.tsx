5d4770b5d00afd4bec48983d16df332cb8c24c6d
imsaidahmadovich <imsaidahmadovich@gmail.com>
2026-02-03 05:16:25 +0500

    Remove ads script from leaderboard page
    Removed Google Ads script from LeaderboardPage.

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -146,6 +146,5 @@ function LeaderboardPage() {
     </div>
   );
 }
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6493634839455307"
-     crossorigin="anonymous"></script>
+
 export default withAuth(LeaderboardPage);
