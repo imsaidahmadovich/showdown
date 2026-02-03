ea14de7077fc43e94bb085293fad8a05694d3861
imsaidahmadovich <imsaidahmadovich@gmail.com>
2026-02-03 05:22:35 +0500

    Update page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -337,6 +337,5 @@ function ShopPage() {
         </div>
     );
 }
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6493634839455307"
-     crossorigin="anonymous"></script>
+
 export default withAuth(ShopPage);
